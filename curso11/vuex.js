4edb46103751958fae6c0b34d6a05cc5d77f2700
Carlos Alcala <carlos.alcala@me.com>
2020-05-06 23:50:51 -0400

    Implementacion Curso #11

---
+++ curso11/vuex.js
@@ -0,0 +1,47 @@
+Vue.component('titulo', {
+  template: /*html*/
+  `
+  <div>
+    <h1>Numero: {{ numero }} </h1>
+    <hijo></hijo>
+  </div>
+  `,
+  computed: {
+    // numero(){
+    //   return store.state.numero
+    // }
+    ...Vuex.mapState(['numero'])
+  },
+});
+
+Vue.component('hijo', {
+  template: /*html*/
+  `
+  <div>
+    <button @click="$store.commit('aumentar')">+</button>
+    <h1>Numero: {{ numero }} </h1>  
+  </div>
+  `,
+  computed: {
+    // numero(){
+    //   return store.state.numero
+    // }
+    ...Vuex.mapState(['numero'])
+  },
+});
+
+const store = new Vuex.Store({
+  state: {
+    numero: 10
+  },
+  mutations: {
+    aumentar() {
+      this.state.numero ++
+    }
+  }
+});
+
+new Vue({
+  el: '#app',
+  store: store
+})
